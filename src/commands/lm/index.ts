390d4e0a9bc77f8e3d11a24d617c4092cf3f7ed1
David Calavera <david.calavera@gmail.com>
2019-01-09 09:47:34 -0800

    Show requirements information
    Print the installed versions of Git, Git LFS and Netlify's Credentials Helper

--- src/commands/lm/index.ts
+++ src/commands/lm/index.ts
@@ -1,9 +1,37 @@
 import {Command, flags} from '@oclif/command'
 
+const execa = require('execa')
+
 export default class LmIndex extends Command {
-  static description = 'Print Netlify Large Media configuration'
+  static description = 'Print Netlify Large Media information'
   static examples = ['$ <%- config.bin %> lm']
 
   async run() {
-  }
+    const gitVersion = execa('git', ['--version'])
+    const lfsVersion = execa('git-lfs', ['--version'])
+    const helperVersion = execa('git-credential-netlify', ['version'])
+
+    try {
+      gitVersion.stdout.pipe(process.stdout)
+      await gitVersion
+    } catch (error) {
+      this.log(error.stderr)
+    }
+    this.log("\n\r")
+
+    try {
+      lfsVersion.stdout.pipe(process.stdout)
+      await lfsVersion
+    } catch (error) {
+      this.log(error.stderr)
+    }
+    this.log("\n\r")
+
+    try {
+      helperVersion.stdout.pipe(process.stdout)
+      await helperVersion
+    } catch (error) {
+      this.log(error.stderr)
+    }
+  } 
 }
