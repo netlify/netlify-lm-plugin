67b9ff4200cc68ef6980ac3f6af4ed5be991144a
David Calavera <david.calavera@gmail.com>
2019-01-09 15:53:24 -0800

    Group information commands under tasks.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/commands/lm/index.ts
+++ src/commands/lm/index.ts
@@ -1,37 +1,36 @@
 import {Command, flags} from '@oclif/command'
 
 const execa = require('execa')
+const Listr = require('listr')
 
 export default class LmIndex extends Command {
   static description = 'Print Netlify Large Media information'
   static examples = ['$ <%- config.bin %> lm']
 
   async run() {
-    const gitVersion = execa('git', ['--version'])
-    const lfsVersion = execa('git-lfs', ['--version'])
-    const helperVersion = execa('git-credential-netlify', ['version'])
+    const tasks = new Listr([
+      {
+        title: 'Checking Git version',
+        task: () => checkVersion('git', 'Git is not installed')
+      },
+      {
+        title: 'Checking Git LFS version',
+        task: () => checkVersion('git-lfs', 'Git LFS is not installed')
+      },
+      {
+        title: `Checking Netlify's Git Credentials version`,
+        task: () => checkVersion('git-credential-netlify', `Netlify's Git Credentials helper is not installed`)
+      },
+    ], {concurrent: true})
 
-    try {
-      gitVersion.stdout.pipe(process.stdout)
-      await gitVersion
-    } catch (error) {
-      this.log(error.stderr)
-    }
-    this.log("\n\r")
-
-    try {
-      lfsVersion.stdout.pipe(process.stdout)
-      await lfsVersion
-    } catch (error) {
-      this.log(error.stderr)
-    }
-    this.log("\n\r")
-
-    try {
-      helperVersion.stdout.pipe(process.stdout)
-      await helperVersion
-    } catch (error) {
-      this.log(error.stderr)
-    }
+    tasks.run().catch(err => {})
   } 
+}
+
+function checkVersion(program, error) {
+  return execa.stdout(program, ['--version']).then(result => {
+    if (result === '') {
+      throw new Error(error)
+    }
+  })
 }
