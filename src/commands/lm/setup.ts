1e769d0bb06fc4af0f98a7b53c41b5e27a5a0e83
David Calavera <david.calavera@gmail.com>
2019-01-23 12:00:05 -0800

    Extract configuration functions.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/commands/lm/setup.ts
+++ src/commands/lm/setup.ts
@@ -92,8 +92,8 @@ and configures your Git environment with the right credentials.
 
   async configureUnixInstall() : Promise<void> {
     const helperPath = joinHelperPath()
-    const pathPromise = this.setupUnixPath(helperPath)
-    const configPromise = this.configureGitConfig(helperPath)
+    const pathPromise = setupUnixPath(helperPath)
+    const configPromise = configureGitConfig(helperPath)
 
     await pathPromise
     await configPromise
@@ -113,76 +113,7 @@ and configures your Git environment with the right credentials.
   }
 
   async setupGitConfig() {
-    await this.configureGitConfig(joinHelperPath())
-  }
-
-  async configureGitConfig(helperPath: string) {
-    // Git expects the config path to always use / even on Windows
-    const gitConfigPath = path.join(helperPath, 'git-config').replace(/\\/g, '/')
-    const gitConfigContent = `
-  # This next lines include Netlify's Git Credential Helper configuration in your Git configuration.
-  [include]
-    path = ${gitConfigPath}
-  `
-    const helperConfig = `[credential]
-    helper = netlify
-    useHttpPath = true
-  `
-
-    fs.writeFileSync(path.join(helperPath, 'git-config'), helperConfig)
-    return this.writeConfig('.gitconfig', gitConfigContent)
-  }
-
-  async setupUnixPath(helperPath: string) {
-    let shell = process.env.SHELL
-    if (!shell) {
-      return Promise.reject(new Error('Unable to detect SHELL type, make sure the variable is defined in your environment'))
-    }
-
-    shell = shell.split(path.sep).pop()
-    const pathScript = `${helperPath}/path.${shell}.inc`
-
-    const initContent = `
-  # The next line updates PATH for Netlify's Git Credential Helper.
-  if [ -f '${pathScript}' ]; then source '${pathScript}'; fi
-  `
-
-    switch (shell) {
-      case 'bash':
-        const bashPath = `script_link="$( command readlink "$BASH_SOURCE" )" || script_link="$BASH_SOURCE"
-  apparent_sdk_dir="\$\{script_link%/*}"
-  if [ "$apparent_sdk_dir" == "$script_link" ]; then
-  apparent_sdk_dir=.
-  fi
-  sdk_dir="$( command cd -P "$apparent_sdk_dir" > /dev/null && command pwd -P )"
-  bin_path="$sdk_dir/bin"
-  if [[ ":\$\{PATH}:" != *":\$\{bin_path}:"* ]]; then
-  export PATH=$bin_path:$PATH
-  fi`
-        fs.writeFileSync(pathScript, bashPath)
-        return this.writeConfig('.bashrc', initContent)
-      case 'zsh':
-        fs.writeFileSync(pathScript, 'export PATH=${0:A:h}/bin:$PATH')
-        return this.writeConfig('.zshrc', initContent)
-      default:
-        const error = `Unable to set credential helper in PATH. We don't how to set the path for ${shell} shell.
-  Set the helper path in your environment PATH: ${helperPath}/bin`
-        return Promise.reject(new Error(error))
-    }
-  }
-
-  async writeConfig(name: string, initContent: string) {
-    const configPath = path.join(os.homedir(), name)
-    if (!fs.existsSync(configPath)) {
-      return
-    }
-
-    const content = fs.readFileSync(configPath, 'utf8')
-    if (content.includes(initContent)) {
-      return
-    }
-
-    fs.appendFileSync(configPath, initContent)
+    await configureGitConfig(joinHelperPath())
   }
 }
 
@@ -218,6 +149,75 @@ async function extractFile(file: string) {
   }
 
   await execa('tar', ['-C', dir, '-xzf', file])
+}
+
+async function setupUnixPath(helperPath: string) {
+  let shell = process.env.SHELL
+  if (!shell) {
+    return Promise.reject(new Error('Unable to detect SHELL type, make sure the variable is defined in your environment'))
+  }
+
+  shell = shell.split(path.sep).pop()
+  const pathScript = `${helperPath}/path.${shell}.inc`
+
+  const initContent = `
+# The next line updates PATH for Netlify's Git Credential Helper.
+if [ -f '${pathScript}' ]; then source '${pathScript}'; fi
+`
+
+  switch (shell) {
+    case 'bash':
+      const bashPath = `script_link="$( command readlink "$BASH_SOURCE" )" || script_link="$BASH_SOURCE"
+apparent_sdk_dir="\$\{script_link%/*}"
+if [ "$apparent_sdk_dir" == "$script_link" ]; then
+apparent_sdk_dir=.
+fi
+sdk_dir="$( command cd -P "$apparent_sdk_dir" > /dev/null && command pwd -P )"
+bin_path="$sdk_dir/bin"
+if [[ ":\$\{PATH}:" != *":\$\{bin_path}:"* ]]; then
+export PATH=$bin_path:$PATH
+fi`
+      fs.writeFileSync(pathScript, bashPath)
+      return writeConfig('.bashrc', initContent)
+    case 'zsh':
+      fs.writeFileSync(pathScript, 'export PATH=${0:A:h}/bin:$PATH')
+      return writeConfig('.zshrc', initContent)
+    default:
+      const error = `Unable to set credential helper in PATH. We don't how to set the path for ${shell} shell.
+Set the helper path in your environment PATH: ${helperPath}/bin`
+      return Promise.reject(new Error(error))
+  }
+}
+
+async function writeConfig(name: string, initContent: string) {
+  const configPath = path.join(os.homedir(), name)
+  if (!fs.existsSync(configPath)) {
+    return
+  }
+
+  const content = fs.readFileSync(configPath, 'utf8')
+  if (content.includes(initContent)) {
+    return
+  }
+
+  return fs.appendFile(configPath, initContent, () => {})
+}
+
+async function configureGitConfig(helperPath: string) {
+  // Git expects the config path to always use / even on Windows
+  const gitConfigPath = path.join(helperPath, 'git-config').replace(/\\/g, '/')
+  const gitConfigContent = `
+# This next lines include Netlify's Git Credential Helper configuration in your Git configuration.
+[include]
+  path = ${gitConfigPath}
+`
+  const helperConfig = `[credential]
+  helper = netlify
+  useHttpPath = true
+`
+
+  fs.writeFileSync(path.join(helperPath, 'git-config'), helperConfig)
+  return writeConfig('.gitconfig', gitConfigContent)
 }
 
 function joinHelperPath() {
