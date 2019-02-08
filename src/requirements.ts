114c5d6cb54fa3d6aacdeffeafc516840e722ea1
David Calavera <david.calavera@gmail.com>
2019-02-07 21:52:58 -0800

    Check LFS filters.
    Install the filters if they are not installed with lm:install.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/requirements.ts
+++ src/requirements.ts
@@ -12,37 +12,46 @@ export const GitValidators = [
   }
 ]
 
-export async function checkGitVersion() : Promise<string|Error> {
+export async function checkGitVersion() : Promise<string> {
   try {
     const result = await execa('git', ['--version'])
     return Promise.resolve(result.stdout.split(' ').pop())
   } catch (error) {
-    return Promise.reject(new Error('Check that Git is installed in your system'))
+    throw new Error('Check that Git is installed in your system')
   }
 }
 
-export async function checkLFSVersion() : Promise<string|Error> {
+export async function checkLFSVersion() : Promise<string> {
   try {
     const result = await execa('git-lfs', ['--version'])
     return matchVersion(result.stdout, /git-lfs\/([\.\d]+).*/, '2.5.1', 'Invalid Git LFS version. Please update to version 2.5.1 or above')
   } catch (error) {
-    return Promise.reject(new Error('Check that Git LFS is installed in your system'))
+    throw new Error('Check that Git LFS is installed in your system')
   }
 }
 
-export async function checkHelperVersion() {
+export async function checkLFSFilters() : Promise<boolean> {
+  try {
+    const result = await execa('git', ['config', '--get-regexp', 'filter.lfs'])
+    return Promise.resolve(result.stdout.length > 0)
+  } catch (error) {
+    return Promise.resolve(false)
+  }
+}
+
+export async function checkHelperVersion() : Promise<string> {
   try {
     const result = await execa('git-credential-netlify', ['--version'])
     return matchVersion(result.stdout, /git-credential-netlify\/([\.\d]+).*/, '0.1.1', `Invalid Netlify's Git Credential version. Please update to version 2.5.1 or above`)
   } catch (error) {
-    return Promise.reject(new Error(`Check that Netlify's Git Credential helper is installed and updated to the latest version`))
+    throw new Error(`Check that Netlify's Git Credential helper is installed and updated to the latest version`)
   }
 }
 
-function matchVersion(out: string, regex: RegExp, version: string, message: string) : Promise<string|Error> {
+function matchVersion(out: string, regex: RegExp, version: string, message: string) : Promise<string> {
   const match = out.match(regex)
   if (!match || match.length != 2 || semver.lt(match[1], version)) {
-    return Promise.reject(new Error(message))
+    throw new Error(message)
   }
   return Promise.resolve(match[1])
 }
